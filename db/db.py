6067dcdaddd086e8f5de885b84b24f7d93e6fdb1
josephchenhk <josephchenhk@gmail.com>
2019-09-28 17:34:57 +0800

    changed db to support ORM

--- db/db.py
+++ db/db.py
@@ -8,31 +8,65 @@
 from sqlalchemy import create_engine
 from sqlalchemy import Table
 from sqlalchemy import MetaData
+from sqlalchemy import Column
+from sqlalchemy import Integer
+from sqlalchemy import String
+from sqlalchemy import Date
+from sqlalchemy.ext.declarative import declarative_base
 from datetime import date
+
+from config.config import MYSQL_CONFIG
+
+Base = declarative_base()
 
 class DB:
 
     def create_engine(self, user:str, password:str, address:str, port:int, database:str):
         engine = create_engine(f"mysql+pymysql://{user}:{password}@{address}:{port}/{database}")
-        conn = engine.connect()
         self.engine = engine
+        # 绑定引擎
         self.metadata = MetaData(engine)
-        self.conn = conn
+
+    @property
+    def conn(self):
+        self._conn = self.engine.connect()
+        return self._conn
+
+    def close_conn(self):
+        self._conn.close()
+
+    def create_table(self):
+        # 创建数据表，如果数据表存在则忽视
+        self.metadata.create_all()
+
 
 
 if __name__=="__main__":
-    db = DB()
-    db.create_engine(user="user",
-                     password="pwd",
-                     address="localhost",
-                     port=3306,
-                     database="mydb")
-    results_table = Table('results', db.metadata, autoload=True)
-    res = results_table.select(results_table.c.Date==date(2019,6,23)).execute()
-    # res = results_table.select(results_table.c.Date >= "2019-06-23").execute()
+    # db = DB()
+    # db.create_engine(user="user",
+    #                  password="pwd",
+    #                  address="localhost",
+    #                  port=3306,
+    #                  database="mydb")
+    # results_table = Table('results', db.metadata, autoload=True)
+    # res = results_table.select(results_table.c.Date==date(2019,6,23)).execute()
+    # # res = results_table.select(results_table.c.Date >= "2019-06-23").execute()
+    #
+    # for item in res.fetchall():
+    #     print(item)
 
-    for item in res.fetchall():
-        print(item)
+    db = DB()
+    db.create_engine(**MYSQL_CONFIG.get("horse"))
+
+    # 定义表格
+    fixtures_table = Table('fixtures', db.metadata,
+                       Column('index', Integer, primary_key=True),
+                       Column('Date', Date),
+                       Column('Location', String(2))
+                       )
+
+    db.create_table()
+
 
 
 
