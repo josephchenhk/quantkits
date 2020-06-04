8692b3d88d9a6e6b6c9b175cadcb5e3c0d99fb9f
josephchenhk <josephchenhk@gmail.com>
2020-06-04 16:53:29 +0800

    [add] reset logger

--- quantkits/logger/__init__.py
+++ quantkits/logger/__init__.py
@@ -38,3 +38,51 @@ logger.addHandler(ch)
 # logger.warning('this is a logger warning message')
 # logger.error('this is a logger error message')
 # logger.critical('this is a logger critical message')
+
+
+
+def reset_logger(*args, stream_handler=True, stream_handler_level=logging.DEBUG, **kwargs):
+    """
+    Overwrite the logger settings.
+    :param args:
+    :param stream_handler: default using stream handler
+    :param stream_handler_level: default setting debug mode
+    :param kwargs: should pass something like "file_handler1, file_handler1_level, file_handler2, file_handler2_level"...
+    :return: updated logger according to instructions
+    """
+    # Remove existing handlers
+    for hdlr in logger.handlers[:]:  # remove all old handlers
+        logger.removeHandler(hdlr)
+
+    # Process file handlers
+    params = kwargs.keys()
+    handler_levels = [k for k in params if "level" in k]
+    handler_params = set(params) - set(handler_levels)
+    handlers = [k for k in handler_params if "file_handler" in k]
+
+    fhs = {}
+    for h in handlers:
+        fh = logging.FileHandler(kwargs[h], encoding='utf-8')
+        fh.setLevel(logging.DEBUG)  # set default level to debug
+        fhs[h] = fh
+
+    for hl in handler_levels:
+        h = hl.replace("_level", "") # get corresponding handler
+        if h in fhs:
+            fhs[h].setLevel(kwargs[hl]) # if level is specified, update it
+
+    # Process stream handler
+    sh = logging.StreamHandler()
+    sh.setLevel(stream_handler_level)
+
+    # Set handlers' format (the formatter is given and can not be changed)
+    formatter = logging.Formatter('%(asctime)s - %(filename)s:%(lineno)d - %(name)s - %(levelname)s: %(message)s')
+    for fh in fhs.values():
+        fh.setFormatter(formatter)
+    sh.setFormatter(formatter)
+
+    # Add handlers to logger
+    for fh in fhs.values():
+        logger.addHandler(fh)
+    if stream_handler:
+        logger.addHandler(sh)
